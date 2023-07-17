6e8d172b382f17bdeb64da4f85fb0e8dea1fda09
Karyywy <karyyoungywy@gmail.com>
2023-07-17 17:02:44 +0800

    adding the animation

---
+++ control.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+$mew = $('.mew');
+$eevee = $('.eevee');
+
+$(document).on('keypress', function(e){
+    console.log(e.type + " gives the code " + keyCode);
+
+
+})
+ })
